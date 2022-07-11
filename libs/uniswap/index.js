eb2da01babeab6e0aaad4ea58154d41e36c735de
Abhijit Roy <dalvath3700@gmail.com>
2022-07-11 20:06:33 +0530

    utilized uniswap to check trade price WETH/DAI

---
+++ libs/uniswap/index.js
@@ -0,0 +1,65 @@
+const {
+  ChainId,
+  Fetcher,
+  WETH,
+  Route,
+  Trade,
+  TokenAmount,
+  TradeType,
+} = require("@uniswap/sdk");
+const ethers = require("ethers");
+
+require("dotenv").config({
+  path: require("path").resolve(__dirname, "./.env"),
+});
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+const url = `https://mainnet.infura.io/v3/${INFURA_API_KEY}`;
+const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
+
+const chainId = ChainId.MAINNET;
+const tokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // DAI token address
+
+const fetch_dai_eth = async () => {
+  // fetch dai token data via chainId, tokenAddress, rpc_url
+  const dai = await Fetcher.fetchTokenData(
+    chainId,
+    tokenAddress,
+    customHttpProvider
+  );
+  const weth = WETH[chainId];
+  console.log(`WETH: ${weth.address}`);
+  console.log(`DAI: ${dai.address}`);
+  console.log("-".repeat(45));
+
+  const pair = await Fetcher.fetchPairData(dai, weth, customHttpProvider);
+  const route = new Route([pair], weth);
+  const trade = new Trade(
+    route,
+    new TokenAmount(weth, "100000000000000000"),
+    TradeType.EXACT_INPUT
+  );
+  console.log("Mid Price WETH --> DAI:", route.midPrice.toSignificant(6));
+  // 'midprice' in the context of Uniswap is the price that reflects the ratio
+  // of reserves in one or more pairs. It also represents the price at which
+  // you could theoretically trade a tiny amount (ε) of one token for the other.
+  console.log(
+    "Mid Price DAI --> WETH:",
+    route.midPrice.invert().toSignificant(6)
+  );
+  console.log("-".repeat(45));
+  // 'Execution price' is the ratio of assets sent/received.
+  // The Uniswap swapping of pairs is based on execution price rather than Mid-price.
+  console.log(
+    "Execution Price WETH --> DAI:",
+    trade.executionPrice.toSignificant(6)
+  );
+  console.log(
+    "Mid Price after trade WETH --> DAI:",
+    trade.nextMidPrice.toSignificant(6)
+  );
+};
+
+// Run
+fetch_dai_eth();
